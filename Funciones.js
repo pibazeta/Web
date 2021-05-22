18eee90030c02766740063812aee6c4e159d50b6
agust <agustin.ibazeta@gmail.com>
2021-05-21 21:18:23 -0300

    Web Practica

---
+++ Funciones.js
@@ -0,0 +1,5 @@
+function sumar(){
+    var numero1 =parseInt( document.getElementById("txtNumero1").value);
+    var numero2 =parseInt( document.getElementById("txtNumero2").value);
+    alert("La suma es: " + (numero1+numero2));
+}
